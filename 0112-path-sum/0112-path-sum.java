fc757d738a6459c0b68436f023dde66ade3a48a8
NIKUNJ SORATHIYA <nikunjsorathiya712@gmail.com>
2026-08-29 09:19:24 +0530

    Time: 0 ms (100%), Space: 45.1 MB (31.32%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,8 +16,8 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null){
-            return targetSum == root.val;
+        if(root.left == null && root.right == null && targetSum == root.val){
+            return true;
         }
         else if(hasPathSum(root.left , targetSum - root.val) || hasPathSum(root.right , targetSum - root.val)) return true; 
         return false;
